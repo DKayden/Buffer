d1b44326edd0f38729e9aac9f28449a5b51ca7be
DKayden <dinhmanhhung1504@gmail.com>
2025-03-19 07:31:48 +0700

    Tối ưu chương trình

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ socket_server = SocketServer()
 stop_threads = False
 process_handler = ProccessHandler()
 
+
 def handle_tranfer_magazine(location, line, machine_type, floor):
     process_handler.control_robot_to_location(location)
     print("Robot dang di chuyen toi pickup")
@@ -22,16 +23,20 @@ def handle_tranfer_magazine(location, line, machine_type, floor):
     while not process_handler.check_lift_conveyor(height):
         print("Robot chua dat do cao bang tai")
         # time.sleep(3)
-    stopper_pu_action = LINE_CONFIG.get((line, machine_type, floor), {}).get("stopper_action")
-    process_handler.control_robot_stopper(stopper_pu_action,"open")
-    while not process_handler.check_stopper_robot(stopper_pu_action,"open"):
+    stopper_pu_action = LINE_CONFIG.get((line, machine_type, floor), {}).get(
+        "stopper_action"
+    )
+    process_handler.control_robot_stopper(stopper_pu_action, "open")
+    while not process_handler.check_stopper_robot(stopper_pu_action, "open"):
         print("Stopper chua dung trang thai")
         # time.sleep(3)
     if floor == 1:
-        info_floor = [1,0]
+        info_floor = [1, 0]
     elif floor == 2:
-        info_floor = [0,2]
-    pu_direction = LINE_CONFIG.get((line, machine_type, floor), {}).get("conveyor_direction")
+        info_floor = [0, 2]
+    pu_direction = LINE_CONFIG.get((line, machine_type, floor), {}).get(
+        "conveyor_direction"
+    )
     process_handler.control_robot_conveyor(pu_direction)
     while not process_handler.check_conveyor_robot(pu_direction):
         print("Chua hoan thanh dieu khien bang tai")
@@ -50,11 +55,12 @@ def handle_tranfer_magazine(location, line, machine_type, floor):
     while not process_handler.check_conveyor_robot("stop"):
         print("Chua hoan thanh dieu khien bang tai")
         # time.sleep(6)
-    process_handler.control_robot_stopper(stopper_pu_action,"close")
-    while not process_handler.check_stopper_robot(stopper_pu_action,"close"):
+    process_handler.control_robot_stopper(stopper_pu_action, "close")
+    while not process_handler.check_stopper_robot(stopper_pu_action, "close"):
         print("Stopper chua dung trang thai")
         # time.sleep(3)
-    process_handler.send_message_to_call(location, line, [0,0], machine_type)
+    process_handler.send_message_to_call(location, line, [0, 0], machine_type)
+
 
 def monitor_data():
     """Giám sát và hiển thị dữ liệu nhận được từ server"""
@@ -73,12 +79,12 @@ def monitor_data():
             floor = mission["floor"]
             line = mission["line"]
             machine_type = mission["machine_type"]
-            if machine_type == "loader" :
+            if machine_type == "loader":
                 pick_up_type = "unloader"
                 destination_type = machine_type
             elif machine_type == "unloader":
-                pick_up_type = machine_type
-                destination_type = "loader"
+                pick_up_type = "loader"
+                destination_type = machine_type
             handle_tranfer_magazine(pick_up, line, pick_up_type, floor)
             process_handler.control_robot_to_location(BUFFER_LOCATION)
             print("Robot dang di chuyen toi pickup")
@@ -89,13 +95,15 @@ def monitor_data():
             while not process_handler.check_lift_conveyor(HEIGHT_BUFFER):
                 print("Robot chua dat do cao bang tai")
                 # time.sleep(3)
-            process_handler.control_robot_stopper("cw","open")
-            while not process_handler.check_stopper_robot("cw","open"):
+            process_handler.control_robot_stopper("cw", "open")
+            while not process_handler.check_stopper_robot("cw", "open"):
                 print("Stopper chua dung trang thai")
                 # time.sleep(3)
             while not buffer.buffer_allow_action():
                 print("Buffer chua san sang")
-            buffer_action = LINE_CONFIG.get((line, destination_type, floor), {}).get("buffer_action")
+            buffer_action = LINE_CONFIG.get((line, destination_type, floor), {}).get(
+                "buffer_action"
+            )
             buffer.buffer_action(buffer_action)
             process_handler.control_robot_conveyor("ccw")
             while not process_handler.check_conveyor_robot("ccw"):
@@ -120,8 +128,8 @@ def monitor_data():
             while not process_handler.check_conveyor_robot("stop"):
                 print("Robot chua hoan thanh dieu khien bang tai")
                 # time.sleep(3)
-            process_handler.control_robot_stopper("cw","close")
-            while not process_handler.check_stopper_robot("cw","close"):
+            process_handler.control_robot_stopper("cw", "close")
+            while not process_handler.check_stopper_robot("cw", "close"):
                 print("Stopper chua dung trang thai")
                 # time.sleep(3)
             buffer.robot_confirm_receive_magazine()
@@ -130,17 +138,17 @@ def monitor_data():
             while not process_handler.check_location_robot("LM4"):
                 print("Chua hoan thanh ve standby")
             # process_handler.control_robot_to_location()
-                # Xây dựng thông điệp với giá trị trường là chuỗi bình thường mà không có dấu ngoặc kép thừa
-                # message = {
-                #     "line": i["line"],
-                #     "floor": floor,
-                #     "machine_type": i["machine_type"],
-                # }
+            # Xây dựng thông điệp với giá trị trường là chuỗi bình thường mà không có dấu ngoặc kép thừa
+            # message = {
+            #     "line": i["line"],
+            #     "floor": floor,
+            #     "machine_type": i["machine_type"],
+            # }
 
-                # json_message = json.dumps(message)
+            # json_message = json.dumps(message)
 
-                # Gửi thông điệp dưới dạng chuỗi JSON
-                # server.broadcast_message(json_message)  # Gửi thông điệp dưới dạng chuỗi
+            # Gửi thông điệp dưới dạng chuỗi JSON
+            # server.broadcast_message(json_message)  # Gửi thông điệp dưới dạng chuỗi
 
         # time.sleep(3)  # Đợi 3 giây trước khi kiểm tra lại
 
